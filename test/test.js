40406fe75798f71fb9112b96b3457e432e4e2ff8
emre <emrealplion@gmail.com>
2019-10-26 15:50:54 +0300

    0.0.2

---
+++ test/test.js
@@ -0,0 +1,17 @@
+const NodeEPOSNow = require("../lib/eposnow");
+
+const assert = require("assert");
+const chai = require("chai");
+const expect = chai.expect;
+const should = chai.should();
+
+const accessToken = ""; // <--- Change to your Access Token.
+
+const eposnow = new NodeEPOSNow(accessToken);
+
+describe("node-eposnow initialize", () => {
+  it("should return brands", async () => {
+    const response = await eposnow.v4.brand.list();
+    expect(response).to.be.an("array");
+  });
+});
